85452cf03d81f191a20c1a2ec5db3f02fc0f767f
tharaka5555555555 <113025433+tharaka5555555555@users.noreply.github.com>
2022-09-07 16:17:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94785946099']
+global.premium = ['94785946099']
+global.ownernomer = '94785946099'
+global.ownername = '𝙈𝘼𝙃𝘼𝙕𝙊𝙉𝙀🚫😈'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = '𝙈𝘼𝙃𝘼𝙕𝙊𝙉𝙀🚫😈'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
